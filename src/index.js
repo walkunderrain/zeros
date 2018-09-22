82de359110a1958f61239099ce5f306a25291dac
walkunderrain <39767467+walkunderrain@users.noreply.github.com>
2018-09-22 21:56:37 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 module.exports = function getZerosCount(number) {
-  // your implementation
+  var zero = 0;
+  var a = 5;
+  while (number / a >= 1) {
+    zero += Math.floor(number / a);
+    a *= 5;
+  }
+  return zero;
 }
